ccc157ccd17695eaae957e11bccfd0ee67453946
ktjhouston <ktjhouston@gmail.com>
2019-04-25 15:06:29 -0500

    Added Read_Event and Event struct

--- lamport.c
+++ lamport.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <string.h>
 
 /** Global Variables **/
 
-struct Send_Inst {
+struct Event {
+  int type; //0 => exec; 1 => send; 2 => end; 3 => error
   int sender;
   int receiver;
   char msg[256];
@@ -12,7 +14,7 @@ struct Send_Inst {
 
 /** Function Definitions **/
 
-int Read_Exec();
+struct Event Read_Event();
 
 void Report_End(int rank, int clock);
 
@@ -42,29 +44,55 @@ int main(int argc, char* argv[]){
   if(rank == 0) {
     scanf("%d", &size);
     printf("Number of processes: %d\n", size);
+
+    struct Event e = Read_Event();
+    while(e.type == 0 || e.type == 1) {
+      if(e.type == 0) {
+	printf("\tExec: %d\n", e.sender);
+      }
+      else {
+	printf("\tSend: %d %d %s\n", e.sender, e.receiver, e.msg);
+      }
+      e = Read_Event();
+    }
     
-    for(int i = 0; i < 2; i++) {
-      char tmp[128];
-      scanf("%s", &tmp);
-      printf("%s\n", tmp);
-    }
-    /*
-    for(int i = 0; i < 1; i++) {
-      char* event;
-      int d = scanf("%s", &event);
-      printf("%d: %s.\n", d, event);
-    }
-    */
+    
     /** In child processes, while loop. MSG RECV - if die tag exit, otherwise do stuff **/
   }
   
   MPI_Finalize();
 }
 
-int Read_Exec() {
-  int d;
-  scanf("%d", &d);
-  return d;
+struct Event Read_Event() {
+  struct Event e;
+  e.sender = -1;
+  e.receiver = -1;
+  
+  char ev[10];
+  scanf("%s", &ev); //read event type
+  
+  if( !strcmp(ev, "exec") ) {
+    e.type = 0;
+    scanf("%d", &e.sender);
+    return e;
+  }
+  else if( !strcmp(ev, "send") ){
+    e.type = 1;
+    scanf("%d", &e.sender);
+    scanf("%d", &e.receiver);
+    scanf("%[^\n]", &e.msg);
+    return e;
+    //TODO Read the message to be sent
+  }
+  else if( !strcmp(ev, "end") ) {
+    e.type = 2;
+    return e;
+  }
+  else {
+    e.type = 3;
+    printf("Event type not recognized.\n");
+    return e;
+  }
 }
 
 void Report_End(int rank, int clock) {
